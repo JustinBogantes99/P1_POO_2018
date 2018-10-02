e9d64303b46651e0295d8224c10338965f4c6ab0
Justin Bogantes <justin.bogantes99@gmail.com>
2018-10-02 01:48:50 -0600

    Prueba Main
    una simple prueba de clases

--- Proyecto_1_POO/src/aplicacion/implementacion.java
+++ Proyecto_1_POO/src/aplicacion/implementacion.java
@@ -1,22 +1,24 @@
 package aplicacion;
-//import java.util.ArrayList;
+import java.util.ArrayList;
 /**
  *
  * @author Justin
  */
 public class implementacion {
     public static void main(String args[]){
-        //String nombreCompleto, int telefono, String correo, int cedula, ArrayList<Licencia> licencias
-        //int numero, String fechaEmision, String tipo , String fechaExpiracion
+        
         Licencia nuevaLicencia = new Licencia(201815465,"3/10/18", "B", "3/10/38");
-        //Arraylist<Licencia> listaLicencias = new Arraylist<Licencia>();
-        //Chofer Chofe = new Chofer ( "Justin Bogantes" , 2017110613 , "Justin.bogantes99@gmail.com",  207950816, Arraylist[] )
-        //funcionario = new Funcionario("Carlos Rojas", 1847500);
-         //System.out.println ("Empezamos la ejecución del programa");
-        System.out.println(nuevaLicencia.getNumero());
-        System.out.println(nuevaLicencia.getTipo());
-        System.out.println("");
-        //System.out.println(funcionario.toString());
+        ArrayList <Licencia> nuevaLicencias = new ArrayList <Licencia>();
+        nuevaLicencias.add(nuevaLicencia);
+        Chofer Chofe = new Chofer ( "Justin Bogantes" , 2017110613 , "Justin.bogantes99@gmail.com",  207950816, nuevaLicencias ); 
+        System.out.println(Chofe.getNombreCompleto());
+        System.out.println(Chofe.getTelefono());
+        System.out.println(Chofe.getCorreo());
+        System.out.println(Chofe.getCedula());
+        for (int i=0;i!=nuevaLicencias.size();i++) {
+            //nuevo = Chofe.getLicencias();
+            System.out.println(Chofe.getLicencias());
+        }
     }
 }
 
