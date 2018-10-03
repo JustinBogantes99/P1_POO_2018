61a73f2c90e8efc4fc4465b3fecdfceef2bf457d
SSDesth <carlosmontt1993@gmail.com>
2018-10-02 18:29:18 -0600

    ??? NOS E QUEQUIERE GIT
    sin este sumit git no me deja hacer el pull

--- Proyecto_1_POO/src/aplicacion/Tec.java
+++ Proyecto_1_POO/src/aplicacion/Tec.java
@@ -5,7 +5,9 @@
  */
 package aplicacion;
 
+import java.text.SimpleDateFormat;
 import java.util.*;
+import javax.swing.JOptionPane;
 import javax.xml.bind.annotation.XmlRootElement;
 
 /**
@@ -14,6 +16,7 @@ import javax.xml.bind.annotation.XmlRootElement;
  */
 @XmlRootElement
 public class Tec {
+
     /*----------Variables----------*/
     private List<Secretaria> secretarias;
     private List<Administrador> administradores;
@@ -32,6 +35,7 @@ public class Tec {
     /*----------Setters y Getters----------*/
     /**
      * Metodo Get de la lista secretarias
+     *
      * @return secretarias
      */
     public List<Secretaria> getSecretarias() {
@@ -40,7 +44,8 @@ public class Tec {
 
     /**
      * metodo Set de la lista secretarias
-     * @param secretarias:List 
+     *
+     * @param secretarias:List
      */
     public void setSecretarias(List<Secretaria> secretarias) {
         this.secretarias = secretarias;
@@ -48,6 +53,7 @@ public class Tec {
 
     /**
      * Metodo Get de la lista administradores
+     *
      * @return administradores
      */
     public List<Administrador> getAdministradores() {
@@ -56,6 +62,7 @@ public class Tec {
 
     /**
      * Metodo Set de la lista administradores
+     *
      * @param administradores:List
      */
     public void setAdministradores(List<Administrador> administradores) {
@@ -64,15 +71,17 @@ public class Tec {
 
     /**
      * Metodo Get de la lista vehiculos
+     *
      * @return vehiculos
      */
     public List<Vehiculo> getVehiculos() {
         return vehiculos;
     }
-    
+
     /**
      * Metodo Set de la lista vehiculos
-     * @param vehiculos:List 
+     *
+     * @param vehiculos:List
      */
     public void setVehiculos(List<Vehiculo> vehiculos) {
         this.vehiculos = vehiculos;
@@ -80,6 +89,7 @@ public class Tec {
 
     /**
      * Metodo Get de la lista viajes
+     *
      * @return viajes
      */
     public List<Viaje> getViajes() {
@@ -88,7 +98,8 @@ public class Tec {
 
     /**
      * Metodo Set de la lista viajes
-     * @param viajes:List 
+     *
+     * @param viajes:List
      */
     public void setViajes(List<Viaje> viajes) {
         this.viajes = viajes;
@@ -96,6 +107,7 @@ public class Tec {
 
     /**
      * Metodo Get de la lista choferes
+     *
      * @return choferes
      */
     public List<Chofer> getChoferes() {
@@ -104,7 +116,8 @@ public class Tec {
 
     /**
      * Metodo Set de la lista choferes
-     * @param choferes:List 
+     *
+     * @param choferes:List
      */
     public void setChoferes(List<Chofer> choferes) {
         this.choferes = choferes;
@@ -112,6 +125,7 @@ public class Tec {
 
     /**
      * Metodo Get de la lista usuarios
+     *
      * @return Usuarios
      */
     public List<Pasajero> getUsuarios() {
@@ -120,45 +134,114 @@ public class Tec {
 
     /**
      * Metodo Set de la lista Usuarios
-     * @param Usuarios:List 
+     *
+     * @param Usuarios:List
      */
     public void setUsuarios(List<Pasajero> Usuarios) {
         this.Usuarios = Usuarios;
     }
-    
+
+    /*----------Metodos Especialisados----------*/
     /**
      * Metodo encargado de Agregar un Nuevo usuario y retornar un valor de tipo
      * boolean si se logro agregar exitosamente
+     *
      * @param entrada:Pasajero
      * @return boolean
      */
-    public boolean RegistrarPasajero(Pasajero entrada){
-        if(ValidarPasajero(entrada)){
+    public boolean RegistrarPasajero(Pasajero entrada) {
+        if (ValidarPasajero(entrada)) {
             Usuarios.add(entrada);
             return true;
         }
         return false;
     }
-    
+
     /**
      * Metodo encargado de validar si existe un pasajero con el mismo id del
      * parametro de entrada y si existiera retornaria false
+     *
      * @param entrada:Pasajero
      * @return boolean
      */
-    public boolean ValidarPasajero(Pasajero entrada){
-        for(Pasajero temp:Usuarios){
-            if(temp.getCedula()==entrada.getCedula()){
+    public boolean ValidarPasajero(Pasajero entrada) {
+        for (Pasajero temp : Usuarios) {
+            if (temp.getCedula() == entrada.getCedula()) {
                 return false;
             }
         }
         return true;
     }
-    
-    
-    public boolean SolicitarViaje(Viaje entrada){
-    
+
+    /**
+     * Metodo Que se encargara de agregar una propuesta bde viaje a la lista de
+     * Viajes y de esta manera retornar un true si el viaje se pudo agregar
+     *
+     * @param entrada:Viaje
+     * @return boolean
+     */
+    public boolean SolicitarViaje(Viaje entrada) {
+
         return false;
     }
-}
 
+    public boolean VaidarViaje(Viaje entrada) {
+        //Valida si algun pasajero tiene choque de Fechas con otro Viaje
+        for (Viaje temp : viajes) {
+            for (Pasajero pasajeroEntrada : entrada.getPasajeros()) {
+                for (Pasajero pasajeroRegistrado : temp.getPasajeros()) {
+                    if(pasajeroEntrada.getCedula()==pasajeroRegistrado.getCedula()){
+                       if(ValidarFechasPasajero(entrada, temp)){
+                           JOptionPane.showMessageDialog(null, "El pasajero: " +
+                               pasajeroEntrada.getNombreCompleto() + "\n Cedula: "+
+                               pasajeroEntrada.getCedula()+
+                               "\nPresenta choque con la fecha de otro viaje", 
+                               "ALERTA", 2);
+                           return false;
+                       }
+                        
+                    }
+                }
+            }
+        }
+        //fecha de inicio de solicitud tiene que tener porlomenos 24 horas antes
+        
+        
+
+        return true;
+    }
+
+    /**
+     * Este metodo Valida las fechas de inicio de 2 viajes y revisa si alguna 
+     * de las fechas se traslapan
+     * 
+     * @param viajeNuevo:Viaje
+     * @param viajeExistente:Viaje
+     * @return boolean
+     */
+    public boolean ValidarFechasPasajero(Viaje viajeNuevo, Viaje viajeExistente) {
+        //SimpleDateFormat traductor = new SimpleDateFormat("dd/MM/YYYY");
+        Date fechaInicioViajeNuevo = viajeNuevo.getFechaInicio();
+        Date fechaFinViajeNuevo = viajeNuevo.getFechaFinalizacion();
+        if(fechaInicioViajeNuevo.after(viajeExistente.getFechaInicio()) && 
+                fechaInicioViajeNuevo.before(viajeExistente.getFechaFinalizacion())){
+            return true;
+        }
+        if(fechaFinViajeNuevo.after(viajeExistente.getFechaInicio()) && 
+                fechaFinViajeNuevo.before(viajeExistente.getFechaFinalizacion())){
+            return true;
+        }
+       
+        return false;
+    }
+
+    public boolean ValidarFechaSolicitud(Viaje entrada){
+        Date fechaActrual= new Date();
+        
+        return true;
+    }
+    
+    
+    
+    /*----------Fin de la clase---------*/
+}
--- Proyecto_1_POO/src/aplicacion/implementacion.java
+++ Proyecto_1_POO/src/aplicacion/implementacion.java
@@ -1,12 +1,17 @@
 package aplicacion;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 /**
  *
  * @author Justin
  */
 public class implementacion {
     public static void main(String args[]){
-        
+      /*  
         Licencia nuevaLicencia = new Licencia(201815465,"3/10/18", "B", "3/10/38");
         ArrayList <Licencia> nuevaLicencias = new ArrayList <Licencia>();
         nuevaLicencias.add(nuevaLicencia);
@@ -18,6 +23,20 @@ public class implementacion {
         for (int i=0;i!=nuevaLicencias.size();i++) {
             //nuevo = Chofe.getLicencias();
             System.out.println(Chofe.getLicencias());
+        }*/
+        SimpleDateFormat traductor = new SimpleDateFormat("dd-mm-yyyy");
+        Date fechaActrual= new Date();
+        Date fechatemp; 
+        try {
+            fechatemp = traductor.parse("02-10-2018");
+        
+        System.out.println(traductor.format(fechaActrual));
+        System.out.println(fechatemp);
+        System.out.println(fechaActrual.equals(fechatemp));
+        System.out.println(fechaActrual.before(fechatemp));
+        System.out.println(fechaActrual.after(fechatemp));
+        } catch (ParseException ex) {
+            Logger.getLogger(implementacion.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 }
