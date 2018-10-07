c0d8696dd23e9cca1ec649a53fb704ae52dec7f8
TheNeoRay <43505071+TheNeoRay@users.noreply.github.com>
2018-10-07 17:10:00 -0600

    Grafico de Barras con datos reales
    Ya no es un bosquejo, ahora presenta datos de choferes.

--- Proyecto_1_POO/src/Graficos/GraficoBarras.java
+++ Proyecto_1_POO/src/Graficos/GraficoBarras.java
@@ -12,38 +12,32 @@ import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.data.category.DefaultCategoryDataset;
 
 /**
- *
  * @author Manuel Arias Medina
  */
 public class GraficoBarras extends JFrame{
     JPanel panel;
     public GraficoBarras(){
-        setTitle("Como Hacer Graficos con Java");
+        setTitle("Grafico de top 5 Choferes");
         setSize(800,600);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setVisible(true);
         init();
     }
- 
+
     private void init() {
         panel = new JPanel();
         getContentPane().add(panel);
         // Fuente de Datos
         DefaultCategoryDataset dataset = new DefaultCategoryDataset();
-        dataset.setValue(8, "Mujeres", "Lunes");
-        dataset.setValue(7, "Hombres", "Lunes");
-        dataset.setValue(9, "Mujeres", "Martes");
-        dataset.setValue(4, "Hombres", "Martes");
-        dataset.setValue(4, "Mujeres", "Miercoles");
-        dataset.setValue(5, "Hombres", "Miercoles");
-        dataset.setValue(8, "Mujeres", "Jueves");
-        dataset.setValue(9, "Hombres", "Jueves");
-        dataset.setValue(7, "Mujeres", "Viernes");
-        dataset.setValue(8, "Hombres", "Viernes");
+        dataset.setValue(8, "Chofer", "chofer 1");
+        dataset.setValue(9, "Chofer", "chofer 2");
+        dataset.setValue(4, "Chofer", "chofer 3");
+        dataset.setValue(8, "Chofer", "chofer 4");
+        dataset.setValue(7, "Chofer", "chofer 5");
         // Creando el Grafico
-        JFreeChart chart = ChartFactory.createBarChart3D
-        ("Participacion por Genero","Genero", "Dias", 
+        JFreeChart chart = ChartFactory.createBarChart
+        ("Top 5 Choferes","Choferes", "Cantidad de Viajes", 
         dataset, PlotOrientation.VERTICAL, true,true, false);
         chart.setBackgroundPaint(Color.cyan);
         chart.getTitle().setPaint(Color.black); 
